b50686c92e792f0202b9848261e533f57e2aecbb
nicholashaynes1 <nicholashaynes1@gmail.com>
2016-05-11 11:34:27 -0600

    Made the ship move.

--- SecondGame/Controller/SecondGame.cs
+++ SecondGame/Controller/SecondGame.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
+using SecondGame.Model;
 
 namespace SecondGame.Controller
 {
@@ -12,47 +13,91 @@ namespace SecondGame.Controller
 	/// </summary>
 	public class SecondGame : Game
 	{
-		GraphicsDeviceManager graphics;
-		SpriteBatch spriteBatch;
+		// Keyboard states used to determine key presses
+		KeyboardState currentKeyboardState;
+		KeyboardState previousKeyboardState;
+
+		// Gamepad states used to determine button presses
+		GamePadState currentGamePadState;
+		GamePadState previousGamePadState; 
+
+		// A movement speed for the player
+		float playerMoveSpeed;
+		private GraphicsDeviceManager graphics;
+		private SpriteBatch spriteBatch;
+		private Player player;
 
 		public SecondGame ()
 		{
+			
 			graphics = new GraphicsDeviceManager (this);
 			Content.RootDirectory = "Content";
 		}
 
-		/// <summary>
-		/// Allows the game to perform any initialization it needs to before starting to run.
-		/// This is where it can query for any required services and load any non-graphic
-		/// related content.  Calling base.Initialize will enumerate through any components
-		/// and initialize them as well.
-		/// </summary>
+
 		protected override void Initialize ()
 		{
-			// TODO: Add your initialization logic here
+			player = new Player();
+
+			playerMoveSpeed = 8.0f;
             
 			base.Initialize ();
 		}
 
-		/// <summary>
-		/// LoadContent will be called once per game and is the place to load
-		/// all of your content.
-		/// </summary>
+
 		protected override void LoadContent ()
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch (GraphicsDevice);
 
+			// Load the player resources 
+			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X,GraphicsDevice.Viewport.TitleSafeArea.Y +GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			player.Initialize(Content.Load<Texture2D>("Textures/player"), playerPosition);
 			//TODO: use this.Content to load your game content here 
 		}
 
-		/// <summary>
-		/// Allows the game to run logic such as updating the world,
-		/// checking for collisions, gathering input, and playing audio.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+
+		#region update Region
+		private void UpdatePlayer(GameTime gameTime)
+		{
+
+			// Get Thumbstick Controls
+			player.Position.X += currentGamePadState.ThumbSticks.Left.X *playerMoveSpeed;
+			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y *playerMoveSpeed;
+
+			// Use the Keyboard / Dpad
+			if (currentKeyboardState.IsKeyDown(Keys.Left) ||
+				currentGamePadState.DPad.Left == ButtonState.Pressed)
+			{
+				player.Position.X -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Right) ||
+				currentGamePadState.DPad.Right == ButtonState.Pressed)
+			{
+				player.Position.X += playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Up) ||
+				currentGamePadState.DPad.Up == ButtonState.Pressed)
+			{
+				player.Position.Y -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Down) ||
+				currentGamePadState.DPad.Down == ButtonState.Pressed)
+			{
+				player.Position.Y += playerMoveSpeed;
+			}
+			#endregion
+			// Make sure that the player does not go out of bounds
+			player.Position.X = MathHelper.Clamp(player.Position.X, 0,GraphicsDevice.Viewport.Width - player.Width);
+			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0,GraphicsDevice.Viewport.Height - player.Height);
+		}
 		protected override void Update (GameTime gameTime)
 		{
+			
+
+
+			//Update the player
+
 			// For Mobile devices, this logic will close the Game when the Back button is pressed
 			// Exit() is obsolete on iOS
 			#if !__IOS__ &&  !__TVOS__
@@ -61,7 +106,16 @@ namespace SecondGame.Controller
 			#endif
             
 			// TODO: Add your update logic here
-            
+			previousGamePadState = currentGamePadState;
+			previousKeyboardState = currentKeyboardState;
+
+			// Read the current state of the keyboard and gamepad and store it
+			currentKeyboardState = Keyboard.GetState();
+			currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+
+			UpdatePlayer(gameTime);
+
 			base.Update (gameTime);
 		}
 
@@ -74,7 +128,15 @@ namespace SecondGame.Controller
 			graphics.GraphicsDevice.Clear (Color.DeepPink);
             
 			//TODO: Add your drawing code here
-            
+			// Start drawing
+			spriteBatch.Begin();
+
+			// Draw the Player
+			player.Draw(spriteBatch);
+
+			// Stop drawing
+			spriteBatch.End();
+
 			base.Draw (gameTime);
 		}
 	}
